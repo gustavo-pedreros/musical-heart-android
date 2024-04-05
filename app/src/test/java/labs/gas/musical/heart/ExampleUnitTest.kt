84aa0a5fc72655ef705bda8cf38a60027535f197
Gustavo Pedreros <gustavo.pedreros@gmail.com>
2024-04-05 14:39:29 -0300

    add konsist

--- app/src/test/java/labs/gas/musical/heart/ExampleUnitTest.kt
+++ app/src/test/java/labs/gas/musical/heart/ExampleUnitTest.kt
@@ -1,8 +1,14 @@
 package labs.gas.musical.heart
 
+import androidx.lifecycle.ViewModel
+import com.lemonappdev.konsist.api.Konsist
+import com.lemonappdev.konsist.api.ext.list.withConstructor
+import com.lemonappdev.konsist.api.ext.list.withNameEndingWith
+import com.lemonappdev.konsist.api.ext.list.withParentClassOf
+import com.lemonappdev.konsist.api.verify.assertEmpty
+import com.lemonappdev.konsist.api.verify.assertTrue
+import org.junit.Assert.assertEquals
 import org.junit.Test
-
-import org.junit.Assert.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -13,5 +19,51 @@ class ExampleUnitTest {
     @Test
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
+    }
+
+    @Test
+    fun test() {
+        Konsist
+            .scopeFromProject()
+            .classes()
+            .withNameEndingWith("UseCase")
+            .assertTrue {
+                it.resideInPackage("..domain..")
+            }
+    }
+
+    @Test
+    fun test2() {
+        Konsist
+            .scopeFromProject()
+            .classes()
+            .withNameEndingWith("UseCase")
+            .withConstructor {
+                it.hasParameter { param ->
+                    param.type.name.endsWith("Repository")
+                }
+            }
+    }
+
+    @Test
+    fun test3() {
+        Konsist
+            .scopeFromProject()
+            .classes()
+            .withNameEndingWith("ViewModel")
+            .withConstructor {
+                it.hasParameter { param ->
+                    param.type.name.endsWith("Repository")
+                }
+            }.assertEmpty()
+    }
+
+    @Test
+    fun test4() {
+        Konsist
+            .scopeFromProject()
+            .classes()
+            .withParentClassOf(ViewModel::class)
+            .assertTrue { it.name.endsWith("ViewModel") }
     }
 }
